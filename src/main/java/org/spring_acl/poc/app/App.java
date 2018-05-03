d45d545277e13a4648ee1975e773c1574c1fc52f
Yadav <yadav@Yadavs-iMac.local>
2018-05-03 22:18:24 +0530

    First commit

---
+++ src/main/java/org/spring_acl/poc/app/App.java
@@ -0,0 +1,12 @@
+package org.spring_acl.poc.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
